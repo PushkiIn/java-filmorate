e17a908d4a794d2196321cf79acdb0b57271a880
madoka <obivanyakenobi@gmail.com>
2025-10-06 11:07:51 +0300

    RowMappers

--- src/main/java/ru/yandex/practicum/filmorate/storage/mappers/UserRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mappers/UserRowMapper.java
@@ -1,13 +1,16 @@
 package ru.yandex.practicum.filmorate.storage.mappers;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
-@Component("userRowMapper")
+@Component
+@Qualifier("userRowMapper")
 public class UserRowMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
@@ -16,7 +19,13 @@ public class UserRowMapper implements RowMapper<User> {
         user.setEmail(resultSet.getString("email"));
         user.setLogin(resultSet.getString("login"));
         user.setName(resultSet.getString("name"));
-        user.setBirthday(resultSet.getDate("birthday").toLocalDate());
+        try {
+            LocalDate birthday = resultSet.getDate("birthday").toLocalDate();
+            if (!resultSet.wasNull()) {
+                user.setBirthday(birthday);
+            }
+        } catch (SQLException e) {
+        }
         return user;
     }
 }
